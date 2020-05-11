08b0df5a5d7e3e8ff12a789bce7b570c86f1c439
ilhamSABOUR <ilham.sabour98@hotmail.com>
2020-05-11 19:07:04 +0000

    build

--- project/Block Breaker/Assets/Scripts/MyEditorScript.cs
+++ project/Block Breaker/Assets/Scripts/MyEditorScript.cs
@@ -4,7 +4,13 @@ public class MyEditorScript
 {
     static void PerformBuild()
     {
-        string[] defaultScene = { "Assets/Scenes/StartMenu.unity" };
+        string[] defaultScene = { "Assets/Scenes/StartMenu.unity",
+                                    "Assets/Scenes/Level 1.unity",
+                                    "Assets/Scenes/Level 2.unity",
+                                    "Assets/Scenes/Level 3.unity",
+                                    "Assets/Scenes/Level 4.unity",
+                                    "Assets/Scenes/Level 5.unity",
+                                     "Assets/Scenes/Game Over.unity"};
         BuildPipeline.BuildPlayer(
             defaultScene, "Build/Test.exe",
             BuildTarget.StandaloneWindows64,
