214bd5394515b1cfaa5aa564d8c424bb0203aa58
ilhamSABOUR <ilham.sabour98@hotmail.com>
2020-05-11 18:57:03 +0000

    build

---
+++ project/Block Breaker/Assets/Scripts/MyEditorScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public class MyEditorScript
+{
+    static void PerformBuild()
+    {
+        string[] defaultScene = { "Assets/Scenes/StartMenu.unity" };
+        BuildPipeline.BuildPlayer(
+            defaultScene, "Build/Test.exe",
+            BuildTarget.StandaloneWindows64,
+            BuildOptions.None);
+
+    }
+}
